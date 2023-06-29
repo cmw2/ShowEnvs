0959636fae775a21d8e0abb4595213db483b2c4f
Chris Westbrook <chwestbr@microsoft.com>
2023-06-29 08:30:31 -0400

    initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShowEnvs.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _config;
+    public string? EnvironmentName { get; set; } = null!;
+    public string? Secret { get; set; }
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
+    {
+        _logger = logger;
+        _config = config;
+    }
+
+    public void OnGet()
+    {
+        this.EnvironmentName = _config["ASPNETCORE_ENVIRONMENT"];
+        this.Secret = _config["Secret"];
+    }
+}
